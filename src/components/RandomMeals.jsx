6785a8ca60eb43a5874e6f9c5fac3e654428c124
Aaron <aaron1venema@gmail.com>
2023-02-18 12:02:45 -0600

    random meal

---
+++ src/components/RandomMeals.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import MealCard from "../components/MealCard";
+import Spinner from "../components/Spinner";
+
+const RandomMeals = () => {
+    const [randomMeals, setRandomMeals] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        if(randomMeals.length < 3){
+        try{
+            fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+                .then(res=>res.json())
+                .then(data=> setRandomMeals(prev => [...prev, ...data.meals]));
+        }catch(error){
+            console.log(error);
+        }
+        }
+        setLoading(false);
+    },[randomMeals])
+
+    console.log(randomMeals)
+
+    if(loading){
+        return <Spinner />
+    }
+
+    return ( 
+        <section className="max-w-6xl mx-auto">
+            <h3 className="w-full text-center text-3xl mb-10">Random Meal Selection</h3>
+            <div className="grid grid-col-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 justify-center items-center">
+                { randomMeals && randomMeals.map((meal)=>(
+                    <MealCard 
+                        meal={meal}
+                        key={meal.idMeal}
+                    />
+                ))}
+            </div>
+        </section>
+     );
+}
+ 
+export default RandomMeals;
