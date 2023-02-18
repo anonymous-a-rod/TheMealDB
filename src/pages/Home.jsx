cb31e55c9205d2eb0541e44c7cc0ce50e947cdd1
Aaron <aaron1venema@gmail.com>
2023-02-18 12:58:49 -0600

    categories

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,14 @@
-import { useEffect, useState } from "react";
+import MealCategories from "../components/MealCategories";
 import RandomMeals from "../components/RandomMeals";
 
 
 const Home = () => {
 
-
     return ( 
         <section className="max-w-6xl mx-auto">
             <h1 className="w-full text-center text-4xl mb-10">The MealDB</h1>
             <RandomMeals />
+            <MealCategories />
         </section>
      );
 }
