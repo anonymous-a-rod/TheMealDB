ba91b2de92dcdde8ca4c84f7931988a97efdcd1d
Kelsey Jackson <kelsojacks@outlook.com>
2023-02-19 18:22:33 -0500

    Update Spinner.jsx

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,7 +1,9 @@
+import { CgSpinner } from "react-icons/cg";
+
 const Spinner = () => {
     return ( 
-        <section>
-            loading...
+        <section className='text-3xl flex justify-center mb-8'>
+            <CgSpinner className="animate-spin h-20 w-20"/>
         </section>
      );
 }
