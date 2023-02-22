4b079014821543562467b4e8db07c7f8415f57b6
Aaron <aaron1venema@gmail.com>
2023-02-22 08:48:54 -0600

    Update Meal.jsx

--- src/pages/Meal.jsx
+++ src/pages/Meal.jsx
@@ -9,7 +9,6 @@ const Meal = () => {
   const [loading, setLoading] = useState(true);
   const [tags, setTags] = useState([]);
   const [instructions, setInstructions] = useState([]);
-  const idMeal = useParams();
   const array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
   const param = useParams().idMeal;
 
