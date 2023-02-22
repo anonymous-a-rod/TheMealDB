0a09654b4c50b63284c46476b4880f3708b17fbf
Aaron <aaron1venema@gmail.com>
2023-02-21 22:31:53 -0600

    Update MealByLetter.jsx

--- src/pages/MealByLetter.jsx
+++ src/pages/MealByLetter.jsx
@@ -28,11 +28,11 @@ export default function MealByLetter() {
     <div>{(display)?
         <div className="max-w-6xl mx-auto">
             <h1 className='text-3xl text-center mb-8'>Recipes that start with {letter}</h1>
-            {(Array.isArray(meals))?<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 justify-center items-center'>
+            {(Array.isArray(meals))?<div className=' grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 justify-center items-center'>
                 { 
                     meals.map((item, index)=>{
                         return <div key={item + index} 
-                        className='max-w-xs rounded overflow-hidden shadow-lg bg-white flex flex-col items-center gap-5 cursor-pointer hover:opacity-75'
+                        className='max-w-xs mx-auto overflow-hidden flex flex-col items-center gap-1 cursor-pointer hover:opacity-75'
                         onClick={()=>navigate(`../meal/${item.idMeal}`)}>
                                 <img src={item.strMealThumb} alt={item.strMeal}/>
                                 <p>{item.strMeal}</p>
