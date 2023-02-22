291539f95fb0c64165b171c58d7e976e1e619cc1
Aaron <aaron1venema@gmail.com>
2023-02-22 11:58:22 -0600

    Update About.jsx
    text-center

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,15 +3,15 @@ const About = () => {
         <section className="max-w-6xl mx-auto pl-10 pr-10">
         <h1 className="w-full text-center text-4xl mb-10 font-semibold">About</h1>
 
-        <p className="mb-4">Welcome to our meals database created in React with Tailwind CSS. Our team, 
+        <p className="mb-4 text-center">Welcome to our meals database created in React with Tailwind CSS. Our team, 
         led by Aaron Venema and consisting of Kelsey Jackson, Kamil Watson, Alex Hessler, Michael Plantamura, and Cliffton James, 
         has worked hard to develop this project during our time in the Mimo Bootcamp.</p>
 
-        <p className="mb-4">Our goal was to create a user-friendly database where users can easily search for
+        <p className="mb-4 text-center">Our goal was to create a user-friendly database where users can easily search for
          meals and recipes using the Meals API. We used a combination of React, Tailwind CSS, and other tools and skills 
          we learned in the Mimo Bootcamp to build a sleek and modern user interface.</p>
 
-        <p className="mb-4">Thank you for using our meals database! We hope it helps you find some delicious recipes to try out.</p>
+        <p className="mb-4 text-center">Thank you for using our meals database! We hope it helps you find some delicious recipes to try out.</p>
 
         </section>
      );
