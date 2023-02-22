e2251b8c18375c2517f661c7deeacf6d689417b7
Aaron <aaron1venema@gmail.com>
2023-02-22 09:17:08 -0600

    fix error

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,9 @@
 const About = () => {
     return ( 
-        <section className="max-w-6xl mx-auto">
-            About Page
+        <section className="max-w-6xl mx-auto pl-10 pr-10">
+            <div>
+                
+            </div>
         </section>
      );
 }
