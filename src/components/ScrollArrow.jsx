7ec97dc0575a322660c7cf4461005bb552f7ae0f
Aaron <aaron1venema@gmail.com>
2023-02-26 10:07:16 -0600

    Update ScrollArrow.jsx

--- src/components/ScrollArrow.jsx
+++ src/components/ScrollArrow.jsx
@@ -4,10 +4,10 @@ const ScrollArrow = ({scrollToTop, arrowDisplay}) => {
     return ( 
         <div 
             className={(arrowDisplay)?
-            'flex flex-col items-center fixed bottom-5 right-1 xl:right-10 xl:bottom-20 cursor-pointer z-10 transition-all delay-150':'fixed bottom-5 -right-20 md:bottom-20 md:-right-20 transition-all delay-150'}    
+            'flex flex-col items-center fixed bottom-5 right-1 xl:right-10 xl:bottom-14 cursor-pointer z-10 transition-all delay-150':'fixed bottom-5 -right-20 md:bottom-20 md:-right-20 transition-all delay-150'}    
             onClick={scrollToTop}
         >
-            <TbArrowBigUpLines className="text-3xl sm:text-4xl lg:text-5xl xl:text-7xl"/>
+            <TbArrowBigUpLines className="text-3xl sm:text-4xl lg:text-5xl xl:text-6xl"/>
             <label className="text-sm hidden xl:block">Back to top</label>
         </div>
      );
