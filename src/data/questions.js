35f1eae408979df560b24faa9af138db4cecff6f
Aaron <aaron1venema@gmail.com>
2023-02-26 11:30:59 -0600

    faq

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,24 +1,28 @@
-const  questions = [
+const questions = [
     {
-        question:"How would I learn of cuisines from different origin countries since i'm incredibly boring and only have a diet of varying desserts?", 
-        answer:"We provide a search by County selection to explore any dessert options from any given origin Country!"
+    question: "How can I learn about cuisines from different countries of origin?",
+    answer: "We provide a search-by-country selection to explore options from any given country of origin!"
     },
     {
-        question:"How can I find meal ideas when i'm on strict diet of ice cream?", 
-        answer:"We provide a search by Category selection on our home page including a Dessert section!"
+    question: "How can I find meal ideas when I'm strictly limited to a diet of ice cream?",
+    answer: "We provide a search-by-category selection on our homepage, including a dessert section!"
     },
     {
-        question:"How can I spice up my boring life?", 
-        answer:"We provide a 'Featured Meals' seletion that provides a fun way to explore meals you may not have previously tried!"
+    question: "How can I spice up my boring life?",
+    answer: "We provide a 'Featured Meals' selection that offers a fun way to explore meals you may not have tried before!"
     },
     {
-        question:"What if i'm tired of my diet of desserts and want to experiment with a random ingredient with something other than suger?", 
-        answer:"We provide a search by Random Ingredient section that will surely lead to many different meal options!"
+    question: "What if I'm tired of my dessert-only diet and want to experiment with a random ingredient that isn't sugar?",
+    answer: "We provide a random-ingredient-selection that will surely lead to many different meal options!"
     },
     {
-        question:"What if I only can think of meals by their first letters?", 
-        answer:"We thought of everything! We provide a section at the very bottom of our page for selection of meal specifically by the first letter!"
+    question: "What if I can only think of meals by their first letters?",
+    answer: "We've thought of everything! We provide a section at the very bottom of our page for selecting meals specifically by the first letter!"
+    },
+    {
+        question: "How can I find meal options for a meat-based diet?",
+        answer: "We offer a by-choice-meat-selection that allows you to find meals that include a variety of meat options!"
     }
-]
+];
 
 export default questions
--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -3,29 +3,18 @@ import questions from "../data/questions";
 
 const FAQ = () => {
   return (
-    <div className='mx-auto max-w-6xl'>
+    <div className='max-w-4xl mx-auto py-16 px-10'>
       <h1 className='w-full text-center text-stone-800 text-4xl mb-10 font-semibold'>FAQ</h1>
         <div>
-          <Question
-            question={questions[0].question}
-            answer={questions[0].answer}
-          />
-          <Question
-            question={questions[1].question}
-            answer={questions[1].answer}
-          />
-          <Question
-            question={questions[2].question}
-            answer={questions[2].answer}
-          />
-          <Question
-            question={questions[3].question}
-            answer={questions[3].answer}
-          />
-          <Question
-            question={questions[4].question}
-            answer={questions[4].answer}
-          />
+          { questions && questions.map((question, index)=> {
+            return (
+              <Question
+                key={question.answer + index}
+                question={question.question}
+                answer={question.answer}
+              />
+            )
+          })}
         </div>
     </div>
     
